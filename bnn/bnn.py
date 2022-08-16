16cbc3f7ad748c9f44864fab8c965d2b805268bd
ryanamjad <73199107+ryanamjad@users.noreply.github.com>
2022-08-16 15:43:07 +0500

    Update bnn.py

--- bnn/bnn.py
+++ bnn/bnn.py
@@ -45,10 +45,8 @@ NETWORK_LFCW1A2 = "lfcW1A2"
 
 if os.environ['BOARD'] == 'Ultra96':
 	PLATFORM="ultra96"
-elif os.environ['BOARD'] == 'Pynq-Z1' or os.environ['BOARD'] == 'Pynq-Z2':
-	PLATFORM="pynqZ1-Z2"
 else:
-	raise RuntimeError("Board not supported")
+	PLATFORM="pynqZ1-Z2"
 
 BNN_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 BNN_LIB_DIR = os.path.join(BNN_ROOT_DIR, 'libraries', PLATFORM)
